e1a191c4ce1c0088807c249a38d193cb8bd784a9
Subash <subash_chandra@apple.com>
2016-09-13 09:49:44 -0700

    first commit

--- testingattirbutedLabel/ViewController.swift
+++ testingattirbutedLabel/ViewController.swift
@@ -1,25 +1,38 @@
-//
-//  ViewController.swift
-//  testingattirbutedLabel
-//
-//  Created by Subash Dantuluri on 3/1/16.
 //  Copyright © 2016 Subash Dantuluri. All rights reserved.
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    private var safari = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func submit(sender: AnyObject) {
+        
+        let actualString = " Hello, <a href=\\\"http://www.facebook.com\\\" target=\\\"_blank\\\" target=\"_blank\">facebook</a> How r u?"
+        
+        let firstPart = NSMutableAttributedString(string: actualString.substringToIndex(actualString.rangeOfString("<a ")!.startIndex))
+        
+        let titlePart = actualString.substringWithRange(Range(actualString.rangeOfString("\">")!.endIndex..<actualString.rangeOfString("</a>")!.startIndex))
+        
+        let linkPart = actualString.substringWithRange(Range(actualString.rangeOfString("=\\\"")!.endIndex..<actualString.rangeOfString("\\\" ")!.startIndex))
+        
+        let lastPart = NSMutableAttributedString(string: actualString.substringFromIndex(actualString.rangeOfString("</a>")!.endIndex))
+        
+        let str: NSMutableAttributedString = NSMutableAttributedString(string: titlePart)
+        str.addAttribute(NSLinkAttributeName, value: linkPart, range: NSRange(location: 0, length: str.length))
+        
+        firstPart.appendAttributedString(str)
+        firstPart.appendAttributedString(lastPart)
+        
+        textViewAnswer.attributedText = firstPart
     }
-
-
+    
+    @IBOutlet weak var textViewAnswer: UITextView!
 }
 
